f60e7d7093c94c65e8b789893df5abed50633577
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:33:08 +0530

    Update code5.java

--- Day 1/code5.java
+++ Day 1/code5.java
@@ -1,4 +1,4 @@
-//Write a program to print all numbers from 1 to 100 i.e. 1 2 3 4 5 6 7 . . . 98 99 100
+//program to print all numbers from 1 to 100 i.e. 1 2 3 4 5 6 7 . . . 98 99 100
 public class Main
 {
 	public static void main(String[] args) {
