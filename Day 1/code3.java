ffa71c3d53860d93a72585c0bf54a25eba7530c3
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:28:30 +0530

    Update code3.java

--- Day 1/code3.java
+++ Day 1/code3.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-  Scanner sc= new Scanner(System.in); 
-  System.out.println("a=");
-  int a= sc.nextInt();
-  if(a%2==0)
-      System.out.println("a is even");
-  else
-      System.out.println("a is odd");
+		Scanner sc= new Scanner(System.in); 
+		System.out.println("a=");
+		int a= sc.nextInt();
+		if(a%2==0)
+			System.out.println("a is even");
+		else
+			System.out.println("a is odd");
 	}
 }
 
