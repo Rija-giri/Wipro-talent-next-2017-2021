95f953908302930e234239076c0c927af1f67542
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:21:32 +0530

    Create code3.java

---
+++ Day 1/code3.java
@@ -0,0 +1,15 @@
+//Write a program to accept a number N and print whether the number is EVEN or ODD
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+  Scanner sc= new Scanner(System.in); 
+  System.out.println("a=");
+  int a= sc.nextInt();
+  if(a%2==0)
+      System.out.println("a is even");
+  else
+      System.out.println("a is odd");
+	}
+}
+
