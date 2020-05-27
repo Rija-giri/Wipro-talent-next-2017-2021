e8ae014c8548eb1f7ebb08bd5ed5d3732f28d684
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:27:35 +0530

    Update code2.java

--- Day 1/code2.java
+++ Day 1/code2.java
@@ -1,18 +1,17 @@
-//Write a program to accept two numbers and print the greater value of the two
+//program to accept two numbers and print the greater value of the two
 import java.util.*;
 public class Main
 {
 public static void main(String[] args) {
 	Scanner sc= new Scanner(System.in); 
 	System.out.println("a=");
-  int a= sc.nextInt();
-  Scanner sc1= new Scanner(System.in);
-  System.out.println("b=");
-  int b= sc1.nextInt();
-
-  if(a>b)
-        System.out.println("a is greater");
-  else
-       System.out.println("b is greater");
+	int a= sc.nextInt();
+	Scanner sc1= new Scanner(System.in);
+	System.out.println("b=");
+	int b= sc1.nextInt();
+	if(a>b)
+		System.out.println("a is greater");
+	else
+		System.out.println("b is greater");
 	}
 }
