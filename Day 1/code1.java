08bdfbcad66eef90d9a88a052c50772b6f1464f4
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:26:01 +0530

    Update code1.java

--- Day 1/code1.java
+++ Day 1/code1.java
@@ -1,17 +1,16 @@
-//Write a program to accept a number N and print whether it is positive, negative or zero
+//program to accept a number N and print whether it is positive, negative or zero
 import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-  Scanner sc= new Scanner(System.in); 
-  int n= sc.nextInt();
-        
-  if(n>0) 	
-  System.out.println("positive");
-  else if (n==0)
-  System.out.println("zero");
-  else if (n<0)
-  System.out.println("negative");
+		Scanner sc= new Scanner(System.in); 
+		int n= sc.nextInt();      
+		if(n>0) 	
+			System.out.println("positive");
+		else if (n==0)
+			System.out.println("zero");
+		else if (n<0)
+			System.out.println("negative");
 
 	}
 }
