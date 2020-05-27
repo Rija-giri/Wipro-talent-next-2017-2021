a3d5521fba2f55481a7570ab9cc3c08c8f7c1a49
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:35:00 +0530

    Create code8.java

---
+++ Day 1/code8.java
@@ -0,0 +1,8 @@
+//program to print all numbers backwards from 100 to 0 i.e. 100 99 98 97 96 . . . 4 3 2 1 0
+public class Main
+{
+	  public static void main(String[] args) {
+        for(int i=100;i>=0;i--)
+              System.out.println(i);
+	}
+}
