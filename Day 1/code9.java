75f6e0ef6a89555821196201f5ffe7216f7a45fd
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:43:21 +0530

    Update code9.java

--- Day 1/code9.java
+++ Day 1/code9.java
@@ -3,7 +3,7 @@
 public class Main
 {
 	  public static void main(String[] args) {
-        for(int i=100;i>=1;i-=3)
-            System.out.println(i);
+		  for(int i=100;i>=1;i-=3)
+			  System.out.println(i);
 	}
 }
