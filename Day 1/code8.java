9c5fe2ce8281c12b695579d1e3b5b4a98a3dfca1
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:41:39 +0530

    Update code8.java

--- Day 1/code8.java
+++ Day 1/code8.java
@@ -2,7 +2,7 @@
 public class Main
 {
 	  public static void main(String[] args) {
-        for(int i=100;i>=0;i--)
-              System.out.println(i);
+		  for(int i=100;i>=0;i--)
+			  System.out.println(i);
 	}
 }
