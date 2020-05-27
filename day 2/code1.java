7d3751381cdf848b3f882134fdf18276262a1f79
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 19:16:57 +0530

    Update code1.java

--- day 2/code1.java
+++ day 2/code1.java
@@ -3,21 +3,19 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-	int n,max;
-max=0;
-	int[] a;         //declaration
-            	a = new int[25];      //instantiation
+		int n,max;
+		max=0;
+		int[] a;         	//declaration
+            	a = new int[25];      	//instantiation
             	for(int i=0;i<25;i++){  
                 	Scanner sc = new Scanner(System.in);
                 	n=sc.nextInt();
                 	a[i] = n;     //initialization values 
-            }
-            for(int i=0;i<25;i++)
-                if(a[i]>max)
-                    max=a[i];
-                }
-            
-        System.out.println("max=" +max);
+		}
+		for(int i=0;i<25;i++)
+			if(a[i]>max)
+				max=a[i];            
+		System.out.println("max=" +max);
         
         }
 }
