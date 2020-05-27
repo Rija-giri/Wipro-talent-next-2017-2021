290e33cd85476d924e6c921ab845df38e9598caf
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:33:59 +0530

    Create code7.java

---
+++ Day 1/code7.java
@@ -0,0 +1,8 @@
+//program to print alternate numbers starting from 0 to 100 i.e. 0 2 4 6 8 10 12 . . . 96 98 100
+public class Main
+{
+	  public static void main(String[] args) {
+          for(int i=0;i<=100;i+=2)
+            		System.out.println(i);
+	}
+}
