24d0663ff211b0a047968a77d470460260492cbd
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-27 10:32:52 +0530

    Create code6.java

---
+++ Day 1/code6.java
@@ -0,0 +1,8 @@
+// program to print alternate numbers starting from 1 to 99 i.e. 1 3 5 7 9 11 13 . . . 95 97 99
+public class Main
+{
+	  public static void main(String[] args) {
+     	  for(int i=1;i<=99;i+=2)
+            		System.out.println(i);
+	}
+}
