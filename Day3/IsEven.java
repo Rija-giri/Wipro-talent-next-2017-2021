c101bbedfcd5f4c0dadbe384a65bead9e293da70
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-29 11:58:01 +0530

    Update IsEven.java

--- Day3/IsEven.java
+++ Day3/IsEven.java
@@ -1,46 +1,18 @@
-	//Check whether the given number is even or not
-
-
-
+//Check whether the given number is even or not
 import java.util.*;
-
-
 public class IsEven {
-	
-
 	public static void main(String[] args) {
-	    
 		System.out.println("Enter a number");   //input
-		
 		Scanner sc= new Scanner(System.in); 
-		
 		int n= sc.nextInt();
-		
 		System.out.println(even(n));
-	
 	}
-	
-	
-	
 	static int even(int n) {
-	    
 		int result;
-	
-		
-		
 		if(n%2==0)
-			
-			
 			result=2;
-		
-		
 		else
-          	  	
-			
 			result=1;
-	    
 		return result;
-	
-	
 	}
 }
