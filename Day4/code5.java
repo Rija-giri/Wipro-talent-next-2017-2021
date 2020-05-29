476d46cbf2f39597bdba9b02c2f3403d8a5ee030
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-29 16:34:00 +0530

    Create code5.java

---
+++ Day4/code5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		String str = s.nextLine();
+		
+		str = str.substring(1, str.length() - 1);
+		
+		System.out.println(str);
+	}
+
+}
