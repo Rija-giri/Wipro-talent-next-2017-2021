17933d555a6e5c0d29c9ce04fa4ae8c82954c81d
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-29 11:58:35 +0530

    Update CmdLineStrArg.java

--- Day3/CmdLineStrArg.java
+++ Day3/CmdLineStrArg.java
@@ -1,10 +1,6 @@
-	//Write a Program that accepts two Strings as command line arguments and generate the output
-
+//Write a Program that accepts two Strings as command line arguments and generate the output
 public class CmdLineStrArg {
-    
 	public static void main(String args[]) {
-        
 		System.out.println(args[0] + " technologies " + args[1]);
 	}
-
 }
