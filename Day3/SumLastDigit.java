25054e67a36aac3cf55dfc1230f5c137b256e3b0
Rija-giri <65938786+Rija-giri@users.noreply.github.com>
2020-05-29 11:52:20 +0530

    Update SumLastDigit.java

--- Day3/SumLastDigit.java
+++ Day3/SumLastDigit.java
@@ -1,57 +1,25 @@
-    //Program to add last digit of 2 given numbers
-    
-
-
+//Program to add last digit of 2 given numbers
 import java.util.*;
-
-
-
 public class SumLastDigit {
-    
-
-	public static void main(String[] args){
-        
-		int a,b,l;
-        
-		System.out.println("Enter a number ");  //input1
-        
-		Scanner sc=new Scanner(System.in);
-        
-		a=sc.nextInt();
-        
-		System.out.println("Enter a number ");  //input2
-        
-		Scanner sc1=new Scanner(System.in);
-        
-		b=sc1.nextInt();
-        
-        
-		
-		if(a<0)             //positiveToNegative
-            
-			a=(~(a-1));     
-        
-		if(b<0)
-            
+    	public static void main(String[] args){
+        	int a,b,l;
+        	System.out.println("Enter a number ");  //input1
+        	Scanner sc=new Scanner(System.in);
+        	a=sc.nextInt();
+        	System.out.println("Enter a number ");  //input2
+        	Scanner sc1=new Scanner(System.in);
+        	b=sc1.nextInt();
+        	if(a<0)             //positiveToNegative
+            		a=(~(a-1));     
+        	elif(b<0)
 			b=(~(b-1));
-        
 		System.out.println("Sum of last digit of given 2 numbers is "+addlastdigit(a,b)); 
-    
 	}  
-  
-    
 	static int addlastdigit(int a,int b) {
-        
 		int i,j,result;
-        
 		i=a%10;
-        
 		j=b%10;
-        
 		result=i+j;
-        
 		return result;
-    
 	}
-
 }
